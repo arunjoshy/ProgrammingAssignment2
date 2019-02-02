204cff7f9b866435373688bd7829aa748fe3f238
arunjoshy <arunjoshy@gmail.com>
2019-02-02 23:32:23 +0530

    file is updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The pair of function defined below executes the matrix inverse operation. Through the usage of lexical scoping and 
+## concept of cache variables, repetitive calculation is avoided when inverse of same matrix is being calculated
 
-## Write a short comment describing this function
+## The first function accepts a matrix, stores a copy of the input matrix, and returns a function which calculate inverse ##the inverse and also store it in cache
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	inv <- NULL
+	z <- NULL
+	set_inv <- function(y)	{
+		z <<- y
+		inv <<- solve(y)
+		inv
+	}
+	set_inv
 
 }
 
 
-## Write a short comment describing this function
+## The second function accepts the output of the first function, first check what is the matrix for which it has previously ##calculated the inverse. If the cached inv variable is not null and if the matrix has not changed, then inverse value in ##the cache is returned, else the inverse is computed fresh. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(r, ...) {
+        
+	inv_prev <- get("inv",environment(r))
+	x_prev <- get("z",environment(r))
+	x_prst <- get("x",environment(r))
+	
+	if(!is.null(inv_prev) && (x_prev == x_prst))	{	
+		message("getting cached data")
+		return(inv_prev)
+		}
+
+	r(x_prst)
 }
